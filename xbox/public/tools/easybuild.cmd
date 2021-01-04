30f03ab527907a13132109b1f6e524e75385e9f9
Empyreal96 <64583248+Empyreal96@users.noreply.github.com>
2021-01-04 22:16:44 +0000

    Small Update
    - Separated the easybuild.cmd (mainmenu) from easy-build-xinit.cmd
    - Added 'basic' Postbuild script (xcopybins.cmd) to copy some build files to the bin dir *Very WIP*
    - Fixed a few incorrect vars shown to the user

---
+++ xbox/public/tools/easybuild.cmd
@@ -0,0 +1,148 @@
+@echo off
+cls
+rem Here we set some variables that are not set by Easy-build-xbox.cmd/razzle during load, so we just load them
+if /i "%_BINPLACE_SUBDIR%" == "" call setfre.cmd
+set COMPLEX=1 
+set NODEVKIT=
+set FOCUS=1
+:eb-xbox-mainmenu
+cls
+set ebdrive=%_NTDrive%
+set ebntroot=%_NTBINDIR%
+set ebxbbins=%_BINPLACE_DIR%
+set ebxbtype=%_BINPLACE_SUBDIR%
+cd /d %ebntroot%
+color 02
+echo --------------------------------------------------------------------------------------------
+echo  Empyreal's Easy-Build for XBOX ORIGINAL (Very limited features currently, WIP)
+echo --------------------------------------------------------------------------------------------
+echo  Build User: %_NTUSER%	Build Machine: %COMPUTERNAME%
+echo  Build Root: %ebntroot% 	Razzle Tool Path: %ebntroot%\public\tools
+echo  Postbuild Dir: %ebxbbins%
+echo --------------------------------------------------------------------------------------------
+echo - Release Type: %ebxbtype%  - Version: XBOX %BUILD_PRODUCT% %BUILD_PRODUCT_VER%
+echo --------------------------------------------------------------------------------------------
+echo  Here you can start the build for the XBOX source with/without 
+echo  the CPXXUPD Patches. (Very limited features currently, WIP)
+echo.
+echo ------------------------------------------------------------------------------
+echo  options) Modify Some Build Options.
+echo --------------------------------------------------------------------------------------------
+echo  1) Clean Build (Full err path, delete object files, no checks)
+echo  2) 'Dirty' Build (Full err path, no checks)
+echo  3) Build Specific Directory Only
+echo  b/w) Open Build Error or Warning Logs
+echo --------------------------------------------------------------------------------------------
+echo  4) Start Postbuild (still figuring this stage out)
+echo  6) Drop to Razzle Prompt
+echo.
+echo ____________________________________________________________________________________________
+set /p NTMMENU=Select:
+echo ____________________________________________________________________________________________
+if /i "%NTMMENU%"=="1" goto cleanbuild
+if /i "%NTMMENU%"=="2" goto DirtyBuild
+REM Opens the most recent builds error logs in Notepad
+if /i "%NTMMENU%"=="b" notepad %ebntroot%\private\build.err & goto eb-xbox-mainmenu
+if /i "%NTMMENU%"=="w" notepad %ebntroot%\private\build.wrn & goto eb-xbox-mainmenu
+if /i "%NTMMENU%"=="3" goto SpecificBLD
+if /i "%NTMMENU%"=="4" goto postbuild-placeholder
+if /i "%NTMMENU%"=="p" notepad %_NTPOSTBLD%\build_logs\postbuild.err & goto eb-xbox-mainmenu
+if /i "%NTMMENU%"=="w" notepad %_NTPOSTBLD%\build_logs\postbuild.wrn & goto eb-xbox-mainmenu
+if /i "%NTMMENU%"=="5" goto MakeISOCheck
+if /i "%NTMMENU%"=="6" exit /b
+if /i "%NTMMENU%"=="var" set && pause
+if /i "%NTMMENU%"=="options" goto BuildOptions
+goto eb-xbox-mainmenu
+
+
+:BuildInfo
+echo still working this out..
+pause
+goto eb-xbox-mainmenu
+:cleanbuild
+cls
+cd /d %ebntroot%\private
+build -bcDeFZP
+pause
+goto eb-xbox-mainmenu
+:DirtyBuild
+cls
+cd /d %ebntroot%\private
+build -bDeFZP
+pause
+goto eb-xbox-mainmenu
+
+
+
+:SpecificBLD
+cls
+echo ----------------------------------------------------------------------
+echo This section we can clean build certain components of the source
+echo So if you want to rebuild the Kernel you would type:
+echo.
+echo F:\xbox\private\ntos
+echo ----------------------------------------------------------------------
+echo.
+echo Type full path to folder or type back to return:
+echo.
+set /p userinput=:
+if "%userinput%"=="back" goto eb-xbox-mainmenu
+cd /d %userinput%
+echo BUILD: %CD% STARTED
+echo.
+build -bcDeFZP
+pause
+goto eb-xbox-mainmenu
+
+:postbuild-placeholder
+cls
+echo This is a 'Postbuild' script I made from 'copybins.cmd' and 'copytest.cmd' to
+echo place files in %_NT386TREE% instead of an Xbox Dev Kit.. THIS IS NOT AN 'OFFICIAL' SCRIPT
+echo This script needs your love! Know how to binplace something? Let me know!
+echo.
+echo xcopybins
+pause
+cmd /c xcopybins.cmd&& goto eb-xbox-mainmenu
+
+:BuildOptions
+
+REM Over time I will add more features, first I need to find what to change and how
+REM Suggestions and improvements greatly welcomed here.
+REM
+REM
+if "%COMPLEX%" == "1" set ebcomplex=On
+if "%COMPLEX%" == "" set ebcomplex=Off
+if "%FOCUS%" == "1" set ebfocus=On
+if "%FOCUS%" == "" set ebfocus=Off
+if "%NODEVKIT%" == "1" set ebdevkit=Retail
+if "%NODEVKIT%" == "" set ebdevkit=DevKit
+cls
+echo ----------------------------------------------------------------------
+echo This section we can use to modify various parts of the build process
+echo for example changing Release type, Build type etc
+echo To switch, type the option after the Value:
+echo ----------------------------------------------------------------------
+echo.
+echo (Current:%ebdevkit%) Release: devkit - retail
+echo (Current:%ebxbtype%) Build Type: fre - chk
+echo (Current:%ebcomplex%) Complex Patches: con - coff
+echo (Current:%ebfocus%) Focus Video Tuner: fon - foff
+echo.
+echo I will slowly add more here over time
+echo ----------------------------------------------------------------------
+echo back) Return
+echo.
+set /p bldopt=:
+if /i "%bldopt%"=="devkit" set NODEVKIT=""
+if /i "%bldopt%"=="retail" set NODEVKIT="1"
+if /i "%bldopt%"=="fre" SETFRE && goto BuildOptions
+if /i "%bldopt%"=="chk" SETCHK && goto BuildOptions
+if /i "%bldopt%"=="con" CPXON && goto BuildOptions
+if /i "%bldopt%"=="coff CPXOFF && goto BuildOptions
+if /i "%bldopt%"=="fon" FOCUSON && goto BuildOptions
+if /i "%bldopt%"=="foff" FOCUSOFF && goto BuildOptions
+if /i "%bldopt%"=="showopt" showopt && goto BuildOptions
+if /i "%bldopt%"=="back" goto eb-xbox-mainmenu
+goto BuildOptions
+
+REM
---
+++ xbox/public/tools/xcopybins.cmd
@@ -0,0 +1,71 @@
+@echo off
+REM
+REM Adapted from 'original' copybins.cmd 
+REM Changes are to create folders and copy files to the NT build drive instead of pushing to
+REM a connected Dev Kit Xbox.. 
+REM
+if "" == "%_NT386TREE%" goto usage
+if not "%1" == "" SET _XBOXMACHINE=-x %1
+if "%1" == "" SET _XBOXMACHINE=
+cls
+echo Ready to copy the Xbox's kernel and dashboard from %_NT386TREE%
+pause
+echo.
+echo Backing up old files to "%_NT386TREE%\release_old"
+echo This will remove any current "%_NT386TREE%\release_old" that exists
+echo (Save the folder now if you want to, then continue)
+pause
+REM A very ugly way to Remove old postbuilds, backup the current and set fresh 'release' folder
+cd /d %_NT386TREE%
+if exist "%_NT386TREE%\release_old" rmdir /Q /S "%_NT386TREE%\release_old"
+if exist "%_NT386TREE%\release" xbcp -f -q -y -r %_NT386TREE%\release\ %_NT386TREE%\release_old\
+rmdir /Q /S %_NT386TREE%\release
+if not exist "%_NT386TREE%\release\" mkdir %_NT386TREE%\release\
+if not exist "%_NT386TREE%\release\c\" mkdir %_NT386TREE%\release\c\
+if not exist "%_NT386TREE%\release\e\" mkdir %_NT386TREE%\release\e\
+if not exist "%_NT386TREE%\release\e\dxt" mkdir %_NT386TREE%\release\e\dxt
+if not exist "%_NT386TREE%\release\y\" mkdir %_NT386TREE%\release\y\
+REM Adapted from copytest.cmd
+if not exist "%_NT386TREE%\release\c\tdata\a7049955" mkdir %_NT386TREE%\release\c\tdata\a7049955
+if not exist "%_NT386TREE%\release\c\tdata\103be6d2" mkdir %_NT386TREE%\release\c\tdata\103be6d2
+if not exist "%_NT386TREE%\release\c\devkit" mkdir %_NT386TREE%\release\c\devkit
+REM ------------
+
+REM using the xbcp command in the source, we copy the required files that would be placed to an Xbox, from the %_NT386TREE% dir, into %_NT386TREE%\Release
+rem
+xbcp -f -q -y %_NT386TREE%\boot\xboxrom_dvt4.bin %_NT386TREE%\release\c\xboxrom.bin
+if not "0" == "%errorlevel%" goto end
+xbcp -f -q -y %_NT386TREE%\devkit\xbdm.dll %_NT386TREE%\release\c\
+xbcp -f -q -y %_NT386TREE%\devkit\cydrive.exe %_NT386TREE%\release\e\dxt\cydrive.dxt
+
+xbcp -f -q -y %_NT386TREE%\dump\xshell.xbe %_NT386TREE%\release\y\
+xbcp -f -q -y -r %_NTBINDIR%\private\test\ui\xshell\tdata\ %_NT386TREE%\release\y\
+xbcp -f -q -y %_NTBINDIR%\private\test\ui\xshell\dashboard.xbx %_NT386TREE%\release\c\
+REM Adapted from copytest.cmd
+
+xbcp -q -y %_XBOXMACHINE% %_NT386TREE%\dump\harness.xbe %_NT386TREE%\release\c\devkit
+xbcp -q -y %_XBOXMACHINE% %_NT386TREE%\dump\hwtest.xbe %_NT386TREE%\release\c\devkit
+xbcp -q -y %_XBOXMACHINE% %_NT386TREE%\xboxtest\testini.ini %_NT386TREE%\release\c\tdata\a7049955
+REM 
+echo.
+echo Files have been copied to %_NT386TREE%\release in the layout of Xbox partitions
+timeout /t 5
+cls
+echo.
+echo.
+echo Now starting Recovery HWTest ISO Creation, A window will open..
+echo Please close the window once it's done. (DON'T CLOSE THIS WINDOW)
+timeout /t 5
+REM This just calls the script hwtrec.cmd to attempt to create a recovery iso
+start hwtrec -all
+pause
+echo Finished Opening %_NT386TREE%
+pause 
+explorer %_NT386TREE%
+goto end
+
+:usage
+echo usage: copybins (_NT386TREE must be defined)
+
+:end
+exit /b
